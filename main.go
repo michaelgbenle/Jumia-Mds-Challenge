5db29f454f29d196544159873f245ec1221c859e
Michael <michaelgbenle@gmail.com>
2022-06-14 14:28:06 +0100

    work in progress

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"github.com/michaelgbenle/jumiaMds/router"
 	"log"
+	"os"
 )
 
 func main() {
 	jumia := router.SetupRouter()
-	err := jumia.Run(":2022")
+	port := os.Getenv("PORT")
+	err := jumia.Run(port)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func init() {
+	os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbName, port)
 }
