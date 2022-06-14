392a1df96ed7b41fa1ed12314c188874d7ee8559
Michael <michaelgbenle@gmail.com>
2022-06-14 15:11:20 +0100

    work in progress

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/michaelgbenle/jumiaMds/router"
 	"log"
 	"os"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return
+	}
 	jumia := router.SetupRouter()
 	port := os.Getenv("PORT")
-	err := jumia.Run(port)
+	err = jumia.Run(port)
 	if err != nil {
 		log.Fatal(err)
 	}
