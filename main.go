e7ca6ca11f44cf15aaa7869b79395ce30d8a8a04
Michael <michaelgbenle@gmail.com>
2022-06-14 14:00:22 +0100

    work in progress

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/michaelgbenle/jumiaMds/router"
 	"log"
 )
 
 func main() {
-
-	fmt.Println("starting jumia app")
 	jumia := router.SetupRouter()
 	err := jumia.Run(":2022")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
