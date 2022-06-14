34eda74b5cdc34a394a0ea8b8e55af1ce061085c
Michael <michaelgbenle@gmail.com>
2022-06-14 17:52:00 +0100

    work in progress

--- database/dbMethods.go
+++ database/dbMethods.go
@@ -2,13 +2,10 @@ package database
 
 import (
 	"github.com/michaelgbenle/jumiaMds/models"
-	"gorm.io/gorm"
 )
-
-var Db *gorm.DB
 
 func GetProductSku(sku string) models.Product {
 	product := models.Product{}
-	Db.Where()
+	Db.Where("sku= ?", sku)
 	return product
 }
