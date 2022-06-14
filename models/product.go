d89fb2b024c534b29e1822d9918293c9043510bf
Michael <michaelgbenle@gmail.com>
2022-06-14 22:44:16 +0100

    work in progress

--- models/product.go
+++ models/product.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Product struct {
 	gorm.Model
 	Country string `json:"country"`
-	Sku     string `json:"sku"`
+	Sku     string `gorm:"index:idx_skucountry,priority:1" json:"sku"`
 	Name    string `json:"name"`
 	Stock   int    `json:"stock"`
 }
