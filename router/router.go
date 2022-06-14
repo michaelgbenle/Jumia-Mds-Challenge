156d7271053cfbf4338f92e4e9d06cc2a3dd7090
Michael <michaelgbenle@gmail.com>
2022-06-14 23:22:02 +0100

    work in progress

--- router/router.go
+++ router/router.go
@@ -19,7 +19,7 @@ func SetupRouter() *gin.Engine {
 	apirouter.GET("/", handlers.SampleRequest)
 	apirouter.GET("/product", handlers.GetProductBySku)
 	apirouter.GET("/product/consume", handlers.ConsumeStock)
-	apirouter.GET("/product/bulkupdate", handlers.BulkUpdate)
+	apirouter.POST("/product/bulkupdate", handlers.BulkUpdate)
 
 	return router
 }
