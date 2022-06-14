1ea50cdc32a24f71ad75614fd94608e8cc838096
Michael <michaelgbenle@gmail.com>
2022-06-14 14:29:37 +0100

    work in progress

--- main.go
+++ main.go
@@ -17,11 +17,11 @@ func main() {
 }
 
 func init() {
-	os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT")
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbName, port)
 }
