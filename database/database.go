16172aa77dd218dcabbfcd8ea8689dbb151df31d
Michael <michaelgbenle@gmail.com>
2022-06-14 14:55:08 +0100

    work in progress

--- database/database.go
+++ database/database.go
@@ -1,9 +1,30 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/michaelgbenle/jumiaMds/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
 
 var Db *gorm.DB
 
 func SetupDb() {
 
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbName, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	Db = db
+
+	Db.AutoMigrate(models.Order{}, models.Product{})
 }
--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/michaelgbenle/jumiaMds/models"
 	"github.com/michaelgbenle/jumiaMds/router"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"log"
 	"os"
 )
-
-var Db *gorm.DB
 
 func main() {
 	jumia := router.SetupRouter()
@@ -19,21 +13,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func init() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, user, password, dbName, port)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	Db = db
-
-	Db.AutoMigrate(models.Order{}, models.Product{})
 }
