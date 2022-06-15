438495ae3cee662e9b11a3b82d24db3f1b182c4f
Michael <michaelgbenle@gmail.com>
2022-06-15 23:58:23 +0100

    work in progress

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,12 +1,13 @@
 package handlers
 
-import (
-	"github.com/golang/mock/gomock"
-	"testing"
-)
-
-func TestGetProductBySku(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	//creates a new mock instance
-	//mockDB := mock_database.NewMockDB(ctrl)
-}
+//import (
+//	mock_database ""github.com/michaelgbenle/jumiaMds/database/database/mocks"
+//	"github.com/golang/mock/gomock"
+//	"testing"
+//)
+//
+//func TestGetProductBySku(t *testing.T) {
+//	ctrl := gomock.NewController(t)
+//	//creates a new mock instance
+//	mockDB := mock_database.NewMockDB(ctrl)
+//}
