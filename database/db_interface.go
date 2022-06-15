dae884b3302af588accf61f828a964dff3577ec3
Michael <michaelgbenle@gmail.com>
2022-06-15 17:23:27 +0100

    work in progress

--- database/db_interface.go
+++ database/db_interface.go
@@ -4,4 +4,7 @@ import "github.com/michaelgbenle/jumiaMds/models"
 
 type DB interface {
 	GetProductSku(sku, country string) models.Product
+	SellStock(product *models.Product) models.Order
+	SellOrCreate(product *models.Product)
+	ProductCreate(product *models.Product) *models.Product
 }
