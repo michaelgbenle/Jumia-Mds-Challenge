767ceff5970dfca60fcfb5d763dc1a391ab2629e
Michael <michaelgbenle@gmail.com>
2022-06-15 13:20:42 +0100

    work in progress

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -36,7 +36,7 @@ func ConsumeStock(c *gin.Context) {
 	})
 }
 
-func BulkUpdate(c *gin.Context) {
+func BulkUpdateFromCsv(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Bulk update successful",
