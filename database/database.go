355c88a5169fb2ecaa1ba68fcf7bb84a017194c7
Michael <michaelgbenle@gmail.com>
2022-06-15 17:08:58 +0100

    work in progress

--- database/database.go
+++ database/database.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"math"
 	"os"
+	"strconv"
+	"strings"
 )
 
 //PostgresDb implements the DB interface
@@ -96,4 +98,39 @@ func (pdb *PostgresDb) ProductCreate(product *models.Product) *models.Product {
 	product.Stock += changeInStock
 	trans.Commit()
 	return product
+}
+
+func BulkUpload(file [][]string) {
+	dbChan := make(chan int, 90)
+	for i, fileLine := range file {
+		if i == 0 {
+			continue
+		}
+		result := strings.ReplaceAll(fileLine[0], `","`, "!")
+		resultArray := strings.Split(result, "!")
+		if len(resultArray) != 4 {
+			log.Println("incomplete data")
+			continue
+		}
+		stock, err := strconv.Atoi(resultArray[3])
+		if err != nil {
+			log.Println("incorrect value")
+			continue
+		}
+		product := &models.Product{
+			Country: resultArray[0],
+			Sku:     resultArray[1],
+			Name:    resultArray[2],
+			Stock:   stock,
+		}
+		wg.Add(1)
+		dbChan <- 1
+		go func(product models.Product) {
+			sellOrCreate(&product)
+			wg.Done()
+			<-dbChan
+
+		}(*product)
+	}
+	//wg.Wait()
 }
--- database/dbMethods.go
+++ database/dbMethods.go
@@ -1,46 +1,7 @@
 package database
 
 import (
-	"github.com/michaelgbenle/jumiaMds/models"
-	"log"
-	"strconv"
-	"strings"
 	"sync"
 )
 
 var wg sync.WaitGroup
-
-func BulkUpload(file [][]string) {
-	dbChan := make(chan int, 90)
-	for i, fileLine := range file {
-		if i == 0 {
-			continue
-		}
-		result := strings.ReplaceAll(fileLine[0], `","`, "!")
-		resultArray := strings.Split(result, "!")
-		if len(resultArray) != 4 {
-			log.Println("incomplete data")
-			continue
-		}
-		stock, err := strconv.Atoi(resultArray[3])
-		if err != nil {
-			log.Println("incorrect value")
-			continue
-		}
-		product := &models.Product{
-			Country: resultArray[0],
-			Sku:     resultArray[1],
-			Name:    resultArray[2],
-			Stock:   stock,
-		}
-		wg.Add(1)
-		dbChan <- 1
-		go func(product models.Product) {
-			sellOrCreate(&product)
-			wg.Done()
-			<-dbChan
-
-		}(*product)
-	}
-	//wg.Wait()
-}
