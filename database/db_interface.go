057ec7d9abed6d6b71fd3b26fd5cd23af5ffa61a
Michael <michaelgbenle@gmail.com>
2022-06-16 11:15:21 +0100

    work in progres

--- database/db_interface.go
+++ database/db_interface.go
@@ -4,7 +4,7 @@ import "github.com/michaelgbenle/jumiaMds/models"
 
 type DB interface {
 	GetProductSku(sku, country string) (*models.Product, error)
-	SellStock(product *models.Product) (models.Order, error)
+	SellStock(product *models.Product) (*models.Order, error)
 	SellOrCreate(product *models.Product)
 	ProductCreate(product *models.Product) *models.Product
 	BulkUpload(file [][]string)
